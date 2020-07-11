f8e51ae6a6e5a39959a6c9c8075c635ccbb7fa35
Dominic Torruellas <dom@domtorr.com>
2020-07-11 13:44:36 -0700

    Basic single character token parsing

--- Flox/Scanner.fs
+++ Flox/Scanner.fs
@@ -2,29 +2,44 @@ module Flox.Scanner
 
 open Flox.Token.TokenTypes
 open Flox.CommonLibrary
+open Flox.Location
 
 let addToken tokenType location tokens = Token.make tokenType location :: tokens
 
 let mapTokenResult foundToken location tokens =
     Result.map (addToken foundToken location) tokens
 
-let mapErrorResult error location char tokens =
+let mapErrorResult error char location tokens =
     let err = Error [ErrorSystem.scanError error location char]
     combineResult err tokens cons List.append
 
-let rec scanline location tokens = function
+let rec scanline location tokens currentChar =
+    let mapResultAndMove = function
+        | Ok t -> scanline (nextColumn location) (mapTokenResult t location tokens)
+        | Error (e, c) -> scanline (nextColumn location) (mapErrorResult e c location tokens)
+
+    match currentChar with
     | [] -> tokens |> Result.map List.rev |> Result.mapError List.rev
-    | '(' :: tl -> scanline (Location.nextColumn location) (mapTokenResult LEFT_PAREN location tokens) tl
-    | ')' :: tl -> scanline (Location.nextColumn location) (mapTokenResult RIGHT_PAREN location tokens) tl
-    | '{' :: tl -> scanline (Location.nextColumn location) (mapTokenResult LEFT_BRACE location tokens) tl
-    | '}' :: tl -> scanline (Location.nextColumn location) (mapTokenResult RIGHT_BRACE location tokens) tl
-    | ',' :: tl -> scanline (Location.nextColumn location) (mapTokenResult COMMA location tokens) tl
-    | '.' :: tl -> scanline (Location.nextColumn location) (mapTokenResult DOT location tokens) tl
-    | '-' :: tl -> scanline (Location.nextColumn location) (mapTokenResult MINUS location tokens) tl
-    | '+' :: tl -> scanline (Location.nextColumn location) (mapTokenResult PLUS location tokens) tl
-    | ';' :: tl -> scanline (Location.nextColumn location) (mapTokenResult SEMICOLON location tokens) tl
-    | '*' :: tl -> scanline (Location.nextColumn location) (mapTokenResult STAR location tokens) tl
-    | c :: tl -> scanline (Location.nextColumn location) (mapErrorResult "Unknown input" location c tokens) tl
+    | '(' :: tl -> mapResultAndMove (Ok LEFT_PAREN) tl
+    | ')' :: tl -> mapResultAndMove (Ok RIGHT_PAREN) tl
+    | '{' :: tl -> mapResultAndMove (Ok LEFT_BRACE) tl
+    | '}' :: tl -> mapResultAndMove (Ok RIGHT_BRACE) tl
+    | ',' :: tl -> mapResultAndMove (Ok COMMA) tl
+    | '.' :: tl -> mapResultAndMove (Ok DOT) tl
+    | '-' :: tl -> mapResultAndMove (Ok MINUS) tl
+    | '+' :: tl -> mapResultAndMove (Ok PLUS) tl
+    | ';' :: tl -> mapResultAndMove (Ok SEMICOLON) tl
+    | '*' :: tl -> mapResultAndMove (Ok STAR) tl
+    | '!' :: '=' :: tl -> mapResultAndMove (Ok BANG_EQUAL) tl
+    | '!' :: tl -> mapResultAndMove (Ok BANG) tl
+    | '=' :: '=' :: tl -> mapResultAndMove (Ok EQUAL_EQUAL) tl
+    | '=' :: tl -> mapResultAndMove (Ok EQUAL) tl
+    | '<' :: '=' :: tl -> mapResultAndMove (Ok LESS_EQUAL) tl
+    | '<' :: tl -> mapResultAndMove (Ok LESS) tl
+    | '>' :: '=' :: tl -> mapResultAndMove (Ok GREATER_EQUAL) tl
+    | '>' :: tl -> mapResultAndMove (Ok GREATER) tl
+    | c :: tl -> mapResultAndMove (Error ("Unknown character", c)) tl
+
 
 (*
 So we have an IEnumerable<string> and a string list
